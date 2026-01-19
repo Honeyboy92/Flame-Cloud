2a86103a2fe154cabe2b8bab103a27aa4a57cad5
Honeyboy92 <your-email@example.com>
2026-01-19 22:36:34 +0500

    Add environment variables support for Supabase

--- client/src/supabaseClient.js
+++ client/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://vhsevmcnbsnjzsrkbcnv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc2V2bWNuYnNuanpzcmtic252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NDk3NDcsImV4cCI6MjA4NDMyNTc0N30.x5HL7TUEgoFz8hREcQtHZdp6mcCKovblIxKdoi9HiNE'
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL || 'https://vhsevmcnbsnjzsrkbcnv.supabase.co'
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoc2V2bWNuYnNuanpzcmtic252Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NDk3NDcsImV4cCI6MjA4NDMyNTc0N30.x5HL7TUEgoFz8hREcQtHZdp6mcCKovblIxKdoi9HiNE'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
